7abd49922aa30a6229aa49c0e8490cccac36f662
airRnot1106 <airRnot1106@users.noreply.github.com>
2024-08-30 22:09:39 +0900

    fix(coc): update key mappings for diagnostics navigation

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -91,8 +91,8 @@ return {
 
             -- Use `[g` and `]g` to navigate diagnostics
             -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
-            keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", { silent = true })
-            keyset("n", "]g", "<Plug>(coc-diagnostic-next)", { silent = true })
+            keyset("n", "g[", "<Plug>(coc-diagnostic-prev)", { silent = true })
+            keyset("n", "g]", "<Plug>(coc-diagnostic-next)", { silent = true })
 
             -- GoTo code navigation
             keyset("n", "gd", "<Plug>(coc-definition)", { silent = true })
