f03ad6740e2d1a64fed8f4024c54a018f7523c9f
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-02 21:11:17 +0900

    fix(lsp): enable virtual text for diagnostics display

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -95,7 +95,7 @@ return {
             })
 
             vim.lsp.handlers["textDocument/publishDiagnostics"] =
-                vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+                vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
         end,
     },
 }
