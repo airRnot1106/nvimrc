47882585c8b1779431464a1828c6047ab7d6544c
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-10 23:10:36 +0900

    feat(lsp): change rename keybinding to F2

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,7 +82,7 @@ return {
                     vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
                     vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
                     vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-                    vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+                    vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>")
                     vim.keymap.set("n", "<Leader>.", "<cmd>lua vim.lsp.buf.code_action()<CR>")
                     vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
                     vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
