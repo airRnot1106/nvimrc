d04ce93c363434811d947923846bd20e7af37f71
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 21:11:37 +0900

    style: Comment out clipboard configuration code

--- lua/options.lua
+++ lua/options.lua
@@ -40,17 +40,17 @@ local options = {
 -- clipboard
 set.clipboard:append { "unnamedplus" }
 
-vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-        ["+"] = require("vim.ui.clipboard.osc52").copy "+",
-        ["*"] = require("vim.ui.clipboard.osc52").copy "*",
-    },
-    paste = {
-        ["+"] = require("vim.ui.clipboard.osc52").paste "+",
-        ["*"] = require("vim.ui.clipboard.osc52").paste "*",
-    },
-}
+-- vim.g.clipboard = {
+--     name = "OSC 52",
+--     copy = {
+--         ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+--         ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+--     },
+--     paste = {
+--         ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+--         ["*"] = require("vim.ui.clipboard.osc52").paste "*",
+--     },
+-- }
 
 for k, v in pairs(options) do
     set[k] = v
