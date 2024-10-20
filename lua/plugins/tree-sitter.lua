2c5cbd3886f5223fd6f1cef105bc7a9975b26a74
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2024-10-20 22:59:07 +0900

    feat(treesitter): add nvim-treesitter-context plugin

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -110,4 +110,22 @@ return {
             end
         end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("treesitter-context").setup {
+                enable = true,
+                max_lines = 5,
+                min_window_height = 0,
+                line_numbers = true,
+                multiline_threshold = 20,
+                trim_scope = "outer",
+                mode = "cursor",
+                separator = nil,
+                zindex = 20,
+                on_attach = nil,
+            }
+        end,
+    },
 }
