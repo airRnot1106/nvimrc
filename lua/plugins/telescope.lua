85873428aedb413fe68fcae7deedb8c2a6aaaf37
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-23 21:31:37 +0900

    feat(telescope): add ui-select extension support

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,8 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "Telescope" },
         config = function()
             local actions = require "telescope.actions"
             require("telescope").setup {
@@ -46,13 +48,41 @@ return {
                         override_file_sorter = true,
                         case_mode = "smart_case",
                     },
+
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                            -- even more opts
+                        },
+
+                        -- pseudo code / specification for writing custom displays, like the one
+                        -- for "codeactions"
+                        -- specific_opts = {
+                        --   [kind] = {
+                        --     make_indexed = function(items) -> indexed_items, width,
+                        --     make_displayer = function(widths) -> displayer
+                        --     make_display = function(displayer) -> function(e)
+                        --     make_ordinal = function(e) -> string
+                        --   },
+                        --   -- for example to disable the custom builtin "codeactions" display
+                        --      do the following
+                        --   codeactions = false,
+                        -- }
+                    },
                 },
             }
             require("telescope").load_extension "fzf"
+            require("telescope").load_extension "ui-select"
         end,
     },
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "Telescope" },
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "Telescope" },
     },
 }
