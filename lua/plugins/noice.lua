db6a3e13859d5c41d1f02883908757cb21b79ebe
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2024-10-25 23:56:03 +0900

    feat(plugins): add noice.nvim configuration and dependencies

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        event = { "VeryLazy" },
+        config = function()
+            require("noice").setup {
+                views = {
+                    cmdline_popup = {
+                        position = {
+                            row = "50%",
+                            col = "50%",
+                        },
+                    },
+                },
+                routes = {
+                    {
+                        filter = {
+                            event = "msg_show",
+                            kind = "",
+                            find = "written",
+                        },
+                        opts = { skip = true },
+                    },
+                },
+                cmdline = {
+                    enabled = true,
+                    view = "cmdline",
+                    format = {
+                        cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+                    },
+                },
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                    },
+                },
+                presets = {
+                    bottom_search = true,
+                    command_palette = true,
+                    long_message_to_split = true,
+                    inc_rename = false,
+                    lsp_doc_border = true,
+                },
+            }
+            require("notify").setup {
+                timeout = 2000,
+                stages = "static",
+            }
+        end,
+    },
+}
