de39b823afc7bcc9d646624dd4f941d91cab51f0
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2024-11-09 12:05:56 +0900

    feat(camel-case-motion): add new key mappings for motions

--- lua/plugins/camel-case-motion.lua
+++ lua/plugins/camel-case-motion.lua
@@ -4,12 +4,18 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             local keymap = vim.api.nvim_set_keymap
-            keymap("o", "iw", "<Plug>CamelCaseMotion_iw", { noremap = true, silent = true })
-            keymap("x", "iw", "<Plug>CamelCaseMotion_iw", { noremap = true, silent = true })
-            keymap("o", "ib", "<Plug>CamelCaseMotion_ib", { noremap = true, silent = true })
-            keymap("x", "ib", "<Plug>CamelCaseMotion_ib", { noremap = true, silent = true })
-            keymap("o", "ie", "<Plug>CamelCaseMotion_ie", { noremap = true, silent = true })
-            keymap("x", "ie", "<Plug>CamelCaseMotion_ie", { noremap = true, silent = true })
+            keymap("n", "[w", "<Plug>CamelCaseMotion_w", { noremap = true, silent = true })
+            keymap("x", "[w", "<Plug>CamelCaseMotion_w", { noremap = true, silent = true })
+            keymap("n", "[b", "<Plug>CamelCaseMotion_b", { noremap = true, silent = true })
+            keymap("x", "[b", "<Plug>CamelCaseMotion_b", { noremap = true, silent = true })
+            keymap("n", "[e", "<Plug>CamelCaseMotion_e", { noremap = true, silent = true })
+            keymap("x", "[e", "<Plug>CamelCaseMotion_e", { noremap = true, silent = true })
+            keymap("o", "ciw", "<Plug>CamelCaseMotion_iw", { noremap = true, silent = true })
+            keymap("x", "ciw", "<Plug>CamelCaseMotion_iw", { noremap = true, silent = true })
+            keymap("o", "cib", "<Plug>CamelCaseMotion_ib", { noremap = true, silent = true })
+            keymap("x", "cib", "<Plug>CamelCaseMotion_ib", { noremap = true, silent = true })
+            keymap("o", "cie", "<Plug>CamelCaseMotion_ie", { noremap = true, silent = true })
+            keymap("x", "cie", "<Plug>CamelCaseMotion_ie", { noremap = true, silent = true })
         end,
     },
 }
