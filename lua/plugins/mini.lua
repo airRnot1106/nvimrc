6188c316c08c46ea9523f0f5ff9e507436e2457d
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-03-22 00:41:09 +0900

    feat(mini): add mini.hipatterns plugin configuration

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -119,4 +119,24 @@ return {
             }
         end,
     },
+    {
+        "echasnovski/mini.hipatterns",
+        version = false,
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local hipatterns = require "mini.hipatterns"
+            local hi_words = require("mini.extra").gen_highlighter.words
+            hipatterns.setup {
+                highlighters = {
+                    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+                    fixme = hi_words({ "FIXME", "Fixme", "fixme" }, "MiniHipatternsFixme"),
+                    hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
+                    todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
+                    note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
+                    -- Highlight hex color strings (`#rrggbb`) using that color
+                    hex_color = hipatterns.gen_highlighter.hex_color(),
+                },
+            }
+        end,
+    },
 }
