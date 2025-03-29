bbddde8f64327f87d5527d64733bd82465dc5709
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-03-29 15:04:38 +0900

    feat(lsp): add LuaJIT runtime configuration and libraries

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,11 +61,21 @@ return {
                 capabilities = capabilities,
                 settings = {
                     Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                            pathStrict = true,
+                            path = { "?.lua", "?/init.lua" },
+                        },
                         diagnostics = {
                             globals = { "vim" },
                         },
                         workspace = {
                             checkThirdParty = false,
+                            library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+                                "${3rd}/luv/library",
+                                "${3rd}/busted/library",
+                                "${3rd}/luassert/library",
+                            }),
                         },
                         format = {
                             enable = false,
@@ -87,10 +97,6 @@ return {
                     },
                 },
             }
-
-            -- lspconfig.tailwindcss.setup {
-            --     capabilities = capabilities,
-            -- }
 
             lspconfig.ts_ls.setup {
                 capabilities = capabilities,
