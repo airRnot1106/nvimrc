e9d17452652ebeb35207455f1b2999351850cf6b
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-05-16 21:32:22 +0900

    chore(gitsigns): set lazy loading to false

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
+        lazy = false,
         config = function()
             require("gitsigns").setup()
         end,
