fa5c4c4bb10bf0a09bd787e6c03f4785f6d5dda5
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-07-22 21:36:10 +0900

    feat: add gleam to conform.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -233,6 +233,7 @@ return {
                 or { "biome-check", "prettierd", "prettier", stop_after_first = true }
             require("conform").setup {
                 formatters_by_ft = {
+                    gleam = { "gleam" },
                     javascript = js_formatters,
                     javascriptreact = js_formatters,
                     lua = { "stylua" },
