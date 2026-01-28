df3ca4558dda1f68e0ad90d484acdef34eb1dddc
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-28 18:09:24 +0900

    feat: add nvim-mado-scratch

---
+++ lua/plugins/nvim-mado-scratch.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "aiya000/nvim-mado-scratch",
+        config = function()
+            require("mado-scratch").setup {
+                -- Optional configuration (these are defaults)
+                file_pattern = {
+                    when_tmp_buffer = "/tmp/mado-scratch-tmp-%d",
+                    when_file_buffer = "/tmp/mado-scratch-file-%d",
+                },
+                default_file_ext = "md",
+                default_open_method = { method = "tabnew" },
+                auto_save_file_buffer = true,
+                use_default_keymappings = true, -- Set to true to enable default keymaps
+                auto_hide_buffer = {
+                    when_tmp_buffer = false,
+                    when_file_buffer = false,
+                },
+            }
+        end,
+    },
+}
