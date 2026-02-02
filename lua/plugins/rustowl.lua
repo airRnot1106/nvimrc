90bef4bbbfe77b0004929894b8d4b83826e706dc
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-02-02 22:35:30 +0900

    feat: add rustowl

---
+++ lua/plugins/rustowl.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "cordx56/rustowl",
+        version = "*", -- Latest stable version
+        build = false,
+        lazy = false, -- This plugin is already lazy
+        opts = {},
+    },
+}
