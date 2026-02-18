49dd9d6591ff08cb0eaa75af56e94ebb9e9d7a21
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-02-18 21:49:19 +0900

    fix: fix issue where oxc-based lsp is not working

--- after/lsp/oxfmt.lua
+++ after/lsp/oxfmt.lua
@@ -2,5 +2,5 @@ local utils = require "utils"
 
 ---@type vim.lsp.Config
 return {
-    cmd = { utils.resolve_local_bin "oxlint", "--lsp" },
+    cmd = { utils.resolve_local_bin "oxfmt", "--lsp" },
 }
