0336f07b8df4ff7a73a7fe6d37243eca498ff269
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-21 00:21:30 +0900

    chore(hop): switch repository to smoka7/hop.nvim

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,7 +1,6 @@
 return {
     {
-        "phaazon/hop.nvim",
-        branch = "v2",
+        "smoka7/hop.nvim",
         config = function()
             require("hop").setup {
                 multi_windows = true,
