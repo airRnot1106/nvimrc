1d146369c0160910a834a03556998045d969d062
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-01 15:46:12 +0900

    feat: add fuzzy-motion plugin configuration

---
+++ lua/plugins/fuzzy-motion.lua
@@ -0,0 +1,13 @@
+return {
+    name = "fuzzy-motion",
+    repo = "yuki-yano/fuzzy-motion.vim",
+    depends = { "kensaku" },
+    on_cmd = { "FuzzyMotion" },
+    on_map = { n = { "<Leader>;" } },
+    lua_add = function()
+        vim.g["fuzzy_motion_matchers"] = { "kensaku", "fzf" }
+    end,
+    lua_source = function()
+        vim.keymap.set("n", "<Leader>;", "<Cmd>FuzzyMotion<CR>", { desc = "Enable FuzzyMotion", silent = true })
+    end,
+}
