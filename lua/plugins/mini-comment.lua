8201aeb5a4a634c3f0f6a96c6245add6f9a5119a
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-01 15:45:44 +0900

    feat: add mini.comment plugin configuration

---
+++ lua/plugins/mini-comment.lua
@@ -0,0 +1,15 @@
+return {
+    name = "mini.comment",
+    repo = "nvim-mini/mini.comment",
+    on_map = { nvo = { "<Leader>/" } },
+    lua_source = function()
+        require("mini.comment").setup {
+            mappings = {
+                comment = "<Leader>/",
+                comment_line = "<Leader>//",
+                comment_visual = "<Leader>/",
+                textobject = "<Leader>/",
+            },
+        }
+    end,
+}
