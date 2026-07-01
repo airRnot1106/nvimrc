bb3a2d075ad2af72b527f44456b75fb4fd5cda64
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-01 15:45:50 +0900

    feat: add skkeleton plugin configuration

---
+++ lua/plugins/skkeleton.lua
@@ -0,0 +1,43 @@
+return {
+    name = "skkeleton",
+    repo = "vim-skk/skkeleton",
+    depends = { "skkeleton_indicator" },
+    on_event = { "InsertEnter", "CmdlineEnter" },
+    lua_add = function()
+        vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "--unstable-kv", "-A" }
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "skkeleton-initialize-pre",
+            callback = function()
+                local skkeleton_data_dir = vim.fn.stdpath "data" .. "/skkeleton"
+                vim.fn.mkdir(skkeleton_data_dir, "p")
+                vim.fn["skkeleton#config"] {
+                    globalDictionaries = {
+                        vim.fn.expand "~/.config/skk/SKK-JISYO.L",
+                    },
+                    eggLikeNewline = true,
+                    keepState = true,
+                    registerConvertResult = true,
+                    sources = { "deno_kv", "google_japanese_input" },
+                    databasePath = skkeleton_data_dir .. "/skkeleton.db",
+                    completionRankFile = skkeleton_data_dir .. "/rank.json",
+                }
+                vim.fn["skkeleton#register_kanatable"]("rom", {
+                    ["jj"] = "escape",
+                })
+            end,
+            group = vim.api.nvim_create_augroup("skkeletonInitPre", { clear = true }),
+        })
+        vim.keymap.set(
+            { "i", "c" },
+            "<C-j>",
+            "<Plug>(skkeleton-enable)",
+            { noremap = false, desc = "Enable skkeleton" }
+        )
+        vim.keymap.set(
+            { "i", "c" },
+            "<C-l>",
+            "<Plug>(skkeleton-disable)",
+            { noremap = false, desc = "Disable skkeleton" }
+        )
+    end,
+}
