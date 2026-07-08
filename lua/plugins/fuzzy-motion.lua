eb3867faa58cb34c9aca618c8cefc1ac88cf7fdd
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-08 09:51:40 +0900

    feat(fuzzy-motion): add visual mode support for FuzzyMotion keymap

--- lua/plugins/fuzzy-motion.lua
+++ lua/plugins/fuzzy-motion.lua
@@ -3,11 +3,16 @@ return {
     repo = "yuki-yano/fuzzy-motion.vim",
     depends = { "kensaku" },
     on_cmd = { "FuzzyMotion" },
-    on_map = { n = { "<Leader>;" } },
+    on_map = { n = { "<Leader>;" }, x = { "<Leader>;" } },
     lua_add = function()
         vim.g["fuzzy_motion_matchers"] = { "kensaku", "fzf" }
     end,
     lua_source = function()
-        vim.keymap.set("n", "<Leader>;", "<Cmd>FuzzyMotion<CR>", { desc = "Enable FuzzyMotion", silent = true })
+        vim.keymap.set(
+            { "n", "x" },
+            "<Leader>;",
+            "<Cmd>FuzzyMotion<CR>",
+            { desc = "Enable FuzzyMotion", silent = true }
+        )
     end,
 }
