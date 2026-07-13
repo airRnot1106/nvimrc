3ae93b899d1933f205c2dbf9ea961826a1508b89
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-13 15:06:56 +0900

    fix(noice): skip E89 error messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -63,6 +63,12 @@ return {
                     },
                     opts = { skip = true },
                 },
+                {
+                    filter = {
+                        find = "E89:",
+                    },
+                    opts = { skip = true },
+                },
             },
             cmdline = {
                 enabled = true,
