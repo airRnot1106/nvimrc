b922ae4d597d3060f0001b2a8f7f829f8a437926
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-08-29 21:25:19 +0900

    feat(vtsls): add css-modules-kit typescript plugin support

--- after/lsp/vtsls.lua
+++ after/lsp/vtsls.lua
@@ -1,12 +1,39 @@
+local npm_root = vim.trim(vim.fn.system "npm root -g")
+local cmk_ts_plugin_location = vim.env.CMK_TS_PLUGIN_LOCATION or npm_root
+local cmk_ts_plugin_available = vim.fn.isdirectory(cmk_ts_plugin_location .. "/@css-modules-kit/ts-plugin") == 1
+
+local filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+}
+
+local global_plugins = {}
+
+if cmk_ts_plugin_available then
+    table.insert(filetypes, "css")
+    table.insert(global_plugins, {
+        name = "@css-modules-kit/ts-plugin",
+        location = cmk_ts_plugin_location,
+        languages = { "css" },
+        enableForWorkspaceTypeScriptVersions = true,
+    })
+end
+
 ---@type vim.lsp.Config
 return {
     on_init = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
     end,
+    filetypes = filetypes,
     settings = {
         vtsls = {
             autoUseWorkspaceTsdk = true,
+            tsserver = {
+                globalPlugins = global_plugins,
+            },
         },
         typescript = {
             format = {
